2a15f3ccb2548f8ea309d87bd98111559a1a5420
Rakhmonali7 <zamonalievr7@gmail.com>
2024-01-18 19:39:26 +0900

    optimesede lc 0034

--- app.js
+++ app.js
@@ -1,10 +1,32 @@
 "use strict";
+// function removeElement(nums, val) {
+//   let k = 0; // Represents the count of elements not equal to val
+
+//   for (let i = 0; i < nums.length; i++) {
+//     if (nums[i] !== val) {
+//       nums[k] = nums[i];
+//       k++;
+//     }
+//   }
+
+//   return k;
+// }
+
+// // Example usage:
+// let nums = [3, 2, 2, 3];
+// let val = 3;
+// let result = removeElement(nums, val);
+
+// console.log(result);
+// console.log(nums.slice(0, result));
 function removeElement(nums, val) {
   let k = 0; // Represents the count of elements not equal to val
 
   for (let i = 0; i < nums.length; i++) {
     if (nums[i] !== val) {
-      nums[k] = nums[i];
+      if (i !== k) {
+        nums[k] = nums[i];
+      }
       k++;
     }
   }
